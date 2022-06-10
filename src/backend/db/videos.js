d91f1a25c1c45fd1bd4af89fe7f34b13d7dec838
srv-pranjal <prns366@gmail.com>
2022-06-10 14:56:39 +0530

    docs: added backend for videos and categories

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,310 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "3UF0ymVdYLA",
+    categoryName: "Stock Market",
+    creator: "Pranjal Kamra",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLSuMlvBBMG0qI6cudqwEcIjoHIO96vl4VSblyMZSQ=s68-c-k-c0x00ffffff-no-rj",
+
+    title:
+      "Stock Market For Beginners | How can Beginners Start Investing in Share Market | Hindi",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Stock Market for Beginners is the introductory video for everyone who is interested in investing in the share market.",
+    duration: "23:05",
+    releaseDate: "Jun 16, 2020",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "Xn7KWR9EOGQ",
+    categoryName: "Stock Market",
+    creator: "CA Rachana Phadke Ranade",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLSA3Tow9_-RTNU6WslhjukZuknVu3JzcGiCMumw5g=s68-c-k-c0x00ffffff-no-rj",
+
+    title:
+      "Basics of Stock Market For Beginners Lecture 1 By CA Rachana Phadke Ranade",
+    description: "Basics of Stock Market For Beginners.",
+    duration: "1:35:07",
+    releaseDate: "Feb 12, 2019",
+  },
+  {
+    _id: "yNaN5kYTNLY",
+    categoryName: "Finance",
+    creator: "warikoo",
+    creatorLogo:
+      "https://yt3.ggpht.com/X9eoDIB9cgb1s-kvATRs1lQDcU4Fjc15NDV9s9FF8ck7IsA8u7OdijaernoDV9LLdePgjlt_=s68-c-k-c0x00ffffff-no-rj",
+
+    title:
+      "Complete Financial Planning for your 20s! | Investing for Beginners 2021 | Ankur Warikoo Hindi",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "How to invest in your 20s? How to pick the right investment option? What are the top investing mistakes to avoid?",
+    duration: "19:12",
+    releaseDate: "Jan 19, 2021",
+  },
+  {
+    _id: "rsFBpGUAZWA",
+    categoryName: "Mutual Funds",
+    creator: "Pranjal Kamra",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLSuMlvBBMG0qI6cudqwEcIjoHIO96vl4VSblyMZSQ=s68-c-k-c0x00ffffff-no-rj",
+
+    title: "What is Mutual funds | Mutual Fund for Beginners in Hindi",
+    description: `If you have no idea about what is mutual funds  you want to learn about mutual fund as a beginner, then this hindi video is for you, you will get the following answers about mutual funds - 
+
+      1. What is mutual funds? Mutual funds types?
+      
+      2.Basics of Mutual Funds?
+      
+      3.How to invest in mutual funds?
+      
+      4. Are Mutual Funds risky ? 
+      
+      5. Which mutual funds are good for beginners ? `,
+    duration: "18:08",
+    releaseDate: "Jun 11, 2019",
+  },
+  {
+    _id: "HixoHQPLFSs",
+    categoryName: "Mutual Funds",
+    creator: "Akshat Shrivastava",
+    creatorLogo:
+      "https://yt3.ggpht.com/VI0OvMwpPQPFGoHAQOHsvtEcFWg-_YzFBhoeohr2CRz1T21Ja4unTGte1Gz6TnDUgVX9wcOG=s68-c-k-c0x00ffffff-no-rj",
+
+    title:
+      "How to invest INR 10,000 per month in a mutual fund? #MutualFunds #InvestmentTips",
+    description: `Mutual Fund can seem a little confusing to novice investors. You might have received a number of calls from agents trying to convince you to invest in their mutual fund, and all that can become too overwhelming!
+
+      So on this video, I try to explain the concept of mutual funds in a very simplified manner`,
+    duration: "26:24",
+    releaseDate: "Jul 9, 2021",
+  },
+  {
+    _id: "t6BQ1Wle8c4",
+    categoryName: "Mutual Funds",
+    creator: "CA Rachana Phadke Ranade",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLSA3Tow9_-RTNU6WslhjukZuknVu3JzcGiCMumw5g=s68-c-k-c0x00ffffff-no-rj",
+
+    title: "Lumpsum or SIP, which is better? | CA Rachana Ranade",
+    description:
+      "Whenever there is a discussion about investments or mutual funds, the topic of Lumpsum or SIP which is better always crops up. In today's video we shall discuss which is better Lumpsum or SIP?",
+    duration: "19:00",
+    releaseDate: "Dec 18, 2021",
+  },
+  {
+    _id: "bP1DhLK3eDs",
+    categoryName: "Mutual Funds",
+    creator: "The Urban Fight",
+    creatorLogo:
+      "https://yt3.ggpht.com/UPRXxY8jCeS0O-guKb6rtVtgbu38dxFfeH8yV2Nc4jNfspYK2fVQ72bbPHqEaBoqE-bT9SxUFgk=s88-c-k-c0x00ffffff-no-rj",
+
+    title:
+      "What are Mutual Funds and How to Select and Buy Mutual Funds in 2022?",
+    description: `The problem with investments is.… Property prices are too high. FDs interest rates are too low. And you don’t want to buy jewellery.
+      But what if I tell you that there is a solution. Yes, I’m talking about Mutual funds. The same one that are subject to market risk, so you need to read the scheme related document carefully before investing. :)
+      
+      But WHAT is this risk? How are people still making money with Mutual Funds? I know you had all these questions and more when you clicked on this video. So ladies and gentlemen, it’s okay if you don’t know anything about Mutual Funds, because in today’s video, we are going to see…
+      
+      Today’s Agenda:
+      
+      1)  What are Mutual Funds and what are the different types?
+      2) 4 Rapid Fire Facts that you must know before getting started.
+      3) A step by step tutorial where I’ll show you how to buy a Mutual Fund using Zerodha’s Coin.
+      4) And towards the end, I’ll give you a Bonus Tip that’ll help you convert your time into actual money. 
+       
+      Just so that we are clear, no Mutual Fund is sponsoring this video. So I don’t care which MF you choose. I’ll just give you the right information, as if I am talking to my younger siblings about investments. And I hope it helps you learn something too.
+      `,
+    duration: "16:00",
+    releaseDate: "Apr 8, 2021",
+  },
+  {
+    _id: "XPe5rY7VqmA",
+    categoryName: "Finance",
+    creator: "Pranjal Kamra",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLSuMlvBBMG0qI6cudqwEcIjoHIO96vl4VSblyMZSQ=s68-c-k-c0x00ffffff-no-rj",
+
+    title:
+      "How to Manage Money and become Rich | 6 Simple steps to Get Rich | Financial Plan",
+    description:
+      "This video will tell you how to get rich through a simple 6 step financial planning that you can do on your own and create wealth. This video will tell you how to create wealth in India through personal finance planning and become rich by following some simple ways to manage your finances. How to manage money and finances",
+    duration: "16:07",
+    releaseDate: "Jun 6, 2019",
+  },
+  {
+    _id: "e0frvHLHMGI",
+    categoryName: "Crypto",
+    creator: "warikoo",
+    creatorLogo:
+      "https://yt3.ggpht.com/X9eoDIB9cgb1s-kvATRs1lQDcU4Fjc15NDV9s9FF8ck7IsA8u7OdijaernoDV9LLdePgjlt_=s68-c-k-c0x00ffffff-no-rj",
+
+    title:
+      "What is the reality of Bitcoin? | Best Cryptocurrency? | Investing for Beginners | Ankur Warikoo",
+    description: `Is investing in bitcoin legal in India?
+
+       Should you invest in bitcoin as a beginner?
+       
+       Friends, today we will talk about bitcoin as a currency, and how it has evolved over the time. We will also address how you can invest in it, what are the pros and cons of it, and if it is advisable for you to invest in it as a beginner?
+       
+       It comes with a lot of advantages and also with a lot of things you need to pay attention to, while investing. 
+       
+       #cryptocurrency #investmenttips
+       
+       This video will explain to you all that, and if there is something you would like to add to what I said in this video, please share that in the comments section.
+       `,
+    duration: "14:37",
+    releaseDate: "Jan 26, 2021",
+  },
+  {
+    _id: "Pbx31ib6gT4",
+    categoryName: "Crypto",
+    creator: "Akshat Shrivastava",
+    creatorLogo:
+      "https://yt3.ggpht.com/VI0OvMwpPQPFGoHAQOHsvtEcFWg-_YzFBhoeohr2CRz1T21Ja4unTGte1Gz6TnDUgVX9wcOG=s68-c-k-c0x00ffffff-no-rj",
+
+    title: "Fundamental Analysis of Cryptocurrencies",
+    description: `Cryptocurrency is a super complex subject. And investment in cryptos can be even more complex. You might invest in bad ones and lose a lot of money, or you might not even invest in any!
+
+      So in this video, I explain one key component that you must understand when you are looking to invest in cryptos. I also throw light on which are the top three cryptos I am investing in and why.`,
+    duration: "20:28",
+    releaseDate: "Aug 1, 2021",
+  },
+  {
+    _id: "Helf3Ku8kho",
+    categoryName: "Crypto",
+    creator: "The Economist",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLSMaTIIQSqXuN0SVp7k-fKJeF6GJC9oBYwXzi5C_QI=s88-c-k-c0x00ffffff-no-rj",
+
+    title: "Crypto: a beginner’s guide | The Economist",
+    description:
+      "Cryptocurrencies, decentralised finance and blockchain technology—what do these terms really mean? The Economist’s finance correspondents guide us through the key concepts of crypto.",
+    duration: "7:46",
+    releaseDate: "Apr 19, 2022",
+  },
+  {
+    _id: "Yb6825iv0Vk",
+    categoryName: "Crypto",
+    creator: "Brian Jung",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLTymjPdhxjlGAaaPsEiB8BeHzNRTR3MmbHtutkHYA=s88-c-k-c0x00ffffff-no-rj",
+
+    title: "How To Invest In Crypto Full Beginners Guide in 2022",
+    description: `This is the ULTIMATE Guide on how to get started in cryptocurrency investing for beginners.
+
+       I recommend signing up for multiple brokerages as mentioned in the video. Some platforms have coins listed on the exchange unlike others and give you the ability to still trade if one goes under for maintenance like we have seen before in the past.`,
+    duration: "19:47",
+    releaseDate: "Jan 7, 2021",
+  },
+  {
+    _id: "Oz9zw7-_vhM",
+    categoryName: "NFT",
+    creator: "Johnny Harris",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLQfM2RzsEcepG6xkKhsp0Qs5n0ktaWWDgrhee-_tg=s88-c-k-c0x00ffffff-no-rj",
+
+    title: "NFTs, Explained",
+    description: "Yes it's hype. But it could change our world.",
+    duration: "17:40",
+    releaseDate: "Apr 27, 2021",
+  },
+  {
+    _id: "Av1wxCEaEcs",
+    categoryName: "NFT",
+    creator: "Study IQ education",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLRo9pdG9dMcifqVxtHZjpjOkiXbealcCPFiBCu4yw=s88-c-k-c0x00ffffff-no-rj",
+
+    title:
+      "What is Non Fungible Token? Difference in Cryptocurrency and NFT explained | Economy & Finance UPSC",
+    description:
+      "What is Non Fungible Token? Difference in Cryptocurrency and NFT explained | Economy & Finance UPSC?",
+    duration: "9:50",
+    releaseDate: "Nov 3, 2021",
+  },
+  {
+    _id: "EjH9GslMCB4",
+    categoryName: "NFT",
+    creator: "Giancarlo buys tokens",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLRd5CjObj29telTXGSaGdUv6tZNgOQ9korM4mBu=s88-c-k-c0x00ffffff-no-rj",
+
+    title: "One year into NFTs — what I wish I knew when I started",
+    description: `In this video I reflect on some key lessons that I've learned after being full time in NFTs for over a year. 
+
+      These are some insights based on my own experience - don't take them as fact and always do your own research. `,
+    duration: "13:42",
+    releaseDate: "Mar 31, 2022",
+  },
+  {
+    _id: "LN61jzaYlIY",
+    categoryName: "NFT",
+    creator: "Sarcastic Sindhi",
+    creatorLogo:
+      "https://yt3.ggpht.com/hnr_6cS2ZzjmZCJd7Kx_P_ajdO9zc3mDM-DiHFCIN-qnVY15Umfyi9Y0nR0XD_4ErQAw4YZfQ_w=s88-c-k-c0x00ffffff-no-r",
+
+    title: "How to Create & Sell Your NFT ! Easiest Method ! (Hindi)",
+    description: `Aaj NFTs ke baare me baat ki hai .. NFT kya hota hai , kaise kaam karta hai ? kaise aap apna NFT bana sakte ho , kaise sell kar sakte ho ....kaise paise kama sakte ho ...`,
+    duration: "13:52",
+    releaseDate: "Jan 14, 2022",
+  },
+  {
+    _id: "Xt-jFF6dlgk",
+    categoryName: "Stock Market",
+    creator: "Akshat Shrivastava",
+    creatorLogo:
+      "https://yt3.ggpht.com/VI0OvMwpPQPFGoHAQOHsvtEcFWg-_YzFBhoeohr2CRz1T21Ja4unTGte1Gz6TnDUgVX9wcOG=s68-c-k-c0x00ffffff-no-rj",
+
+    title:
+      "One of the best stocks to buy RIGHT NOW | Complete Fundamental, Business and Technical Analysis",
+    description: `India is a country where many companies think they have a mass market to cater to. 
+
+      But a big chunk of these firms fails in doing so because of the diversity of this market.
+      
+      In this video, I talk about one company that is a market leader in its domain. And which has been answering the needs of the Indians in a very efficient manner.`,
+    duration: "19:00",
+    releaseDate: "May 22, 2022",
+  },
+  {
+    _id: "qzf_xBbS9uo",
+    categoryName: "Finance",
+    creator: "TechLead",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLTA-k0TzhGuBIVTQ9S0C0Mc6BsIUFLHjz1Np5XDbQ=s88-c-k-c0x00ffffff-no-rj",
+
+    title: "Financial Independence: What no one tells you (as a millionaire)",
+    description:
+      "A lot of people think that 'working' is a means to put a roof over your head, to put food on your table, to support your family.  It's not quite like that really, since after about $20k/year in income, most people already have fulfilled their basic survival needs.  So what is money and financial independence really about?",
+    duration: "10:42",
+    releaseDate: "Jul 23, 2020",
+  },
+  {
+    _id: "7oj6gpAbYgw",
+    categoryName: "Finance",
+    creator: "WION",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLQ5U-RWZZWrWBgS5imVahjSmIJpb21BW3Rwwd1XTw=s88-c-k-c0x00ffffff-no-rj",
+
+    title: "Gravitas Plus | Financial Literacy: The need of the hour",
+    description:
+      "Gravitas Plus | Why do people die with money in the bank? When it comes to money, we either spend too much or are needlessly frugal. WION's Palki Sharma says teaching fundamentals of investment, loans, debt in school is the best way to help people make smart money decisions. Listen in.",
+    duration: "7:27",
+    releaseDate: "Jul 11, 2021",
+  },
+  {
+    _id: "P3Qbbm8T1aY",
+    categoryName: "Stock Market",
+    creator: "Groww",
+    creatorLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLQij4mjnGU5Mkn1EMxVRGsAjdTNjR7pF8-bFngzYQ=s88-c-k-c0x00ffffff-no-rj",
+
+    title: "Market ki Baat with Saurabh Mukherjea & Neeraj Arora | Episode 1",
+    description: `In the first episode of Market ki Baat, Groww ke Saath, host Neeraj Arora will speak to Saurabh   Mukherjea  –  Founder and CIO of Marcellus Investment Managers Pvt Ltd. about his investment philosophy and investment strategy ‘Consistent Compounders'.
+
+      Saurabh shares his views on inflationary concerns, macroeconomic factors, and potential correction in the market due to recession fears. Saurabh also shares some of the key components an investor can look for to analyze the governance of a company, He will share his views on IPOs and how to analyze an IPO stock. 
+      
+      Later Saurabh will share his views on the portfolio of a Groww user and answer some rapid-fire questions. Neeraj will conclude the show by sharing key learnings from today’s conversation according to him.`,
+    duration: "1:02:41",
+    releaseDate: "Jun 5, 2022",
   },
 ];
