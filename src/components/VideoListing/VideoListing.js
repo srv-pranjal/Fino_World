c4609d8bd36a5a45cc5bc404023282609581eb98
srv-pranjal <prns366@gmail.com>
2022-06-13 00:17:27 +0530

    update: updated VideoCard and VideoListing component

--- src/components/VideoListing/VideoListing.js
+++ src/components/VideoListing/VideoListing.js
@@ -1,17 +1,30 @@
 import { useState } from "react";
+import {
+  VideoCard,
+  CategoryChip,
+  PlaylistModal,
+  NothingToShow,
+} from "components";
 import "./VideoListing.css";
-import { CategoryChip, VideoCard, NothingToShow } from "components";
 import { useVideos } from "contexts";
 import { getCategoryFilteredData } from "utils";
 
 export const VideoListing = () => {
+  const [isOpen, setisOpen] = useState(false);
+  const [currentVideo, setCurrentVideo] = useState({});
   const { videoList, categories } = useVideos();
   const [activeCategory, setActiveCategory] = useState("All");
+
+  const openModal = () => setisOpen(true);
+  const closeModal = () => setisOpen(false);
 
   const videosToDisplay = getCategoryFilteredData(videoList, activeCategory);
 
   return (
     <>
+      {isOpen && (
+        <PlaylistModal closeModal={closeModal} currentVideo={currentVideo} />
+      )}
       <div className="videos-container">
         <div className="chips-container">
           <CategoryChip
@@ -33,7 +46,12 @@ export const VideoListing = () => {
         <section className="explore">
           {videosToDisplay.length > 0 ? (
             videosToDisplay.map((video) => (
-              <VideoCard key={video._id} video={video} />
+              <VideoCard
+                key={video._id}
+                video={video}
+                openModal={openModal}
+                setCurrentVideo={setCurrentVideo}
+              />
             ))
           ) : (
             <NothingToShow />
