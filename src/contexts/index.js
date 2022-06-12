d58728c62518f0be5ed46c41ed7e263bae8a0815
srv-pranjal <prns366@gmail.com>
2022-06-13 00:04:45 +0530

    docs: created context for playlists and watchlater

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,4 @@
 export { SidebarProvider, useSidebar } from "./sidebar-context";
 export { VideoProvider, useVideos } from "./video-context";
 export { AuthProvider, useAuth } from "./auth-context";
+export { PlaylistProvider, usePlaylist } from "./playlist-context";
